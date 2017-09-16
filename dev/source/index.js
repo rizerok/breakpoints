897dce8cafd3215b089bab8d2915f6a0b2563271
Alexey S <bashinsky.alexey@gmail.com>
2017-09-16 04:44:06 +0300

    dev second config example

--- dev/source/index.js
+++ dev/source/index.js
@@ -1,48 +1,81 @@
 import SegmentBreakpoints from 'lib/lib';
 import * as fnc from './fncs';
 
+// window.br1 = new SegmentBreakpoints({
+//     0:{
+//         min:false,
+//         in:fnc.b_in0x600,
+//         out:fnc.b_out0x600
+//     },
+//     600:{
+//         max:true,
+//         min:false,
+//         in:fnc.b_in600x700,
+//         out:fnc.b_out600x700
+//     },
+//     700:{
+//         max:true,
+//         min:false,
+//         in:fnc.b_in700x960,
+//         out:fnc.b_out700x960
+//     },
+//     960:{
+//         max:true,
+//         min:false,
+//         in:fnc.b_in960x1280,
+//         out:fnc.b_out960x1280
+//     },
+//     1280:{
+//         max:true,
+//         min:false,
+//         in:fnc.b_in1280x1440,
+//         out:fnc.b_out1280x1440
+//     },
+//     1440:{
+//         max:true,
+//         min:false,
+//         in:fnc.b_in1440x1600,
+//         out:fnc.b_out1440x1600
+//     },
+//     1600:{
+//         max:true,
+//         min:false,
+//         in:fnc.b_in1600,
+//         out:fnc.b_out1600
+//     }
+// });
+
 window.br1 = new SegmentBreakpoints({
     0:{
-        min:false,
         in:fnc.b_in0x600,
         out:fnc.b_out0x600
     },
     600:{
-        max:true,
-        min:false,
         in:fnc.b_in600x700,
         out:fnc.b_out600x700
     },
     700:{
-        max:true,
-        min:false,
         in:fnc.b_in700x960,
         out:fnc.b_out700x960
     },
     960:{
-        max:true,
-        min:false,
         in:fnc.b_in960x1280,
         out:fnc.b_out960x1280
     },
     1280:{
-        max:true,
-        min:false,
         in:fnc.b_in1280x1440,
         out:fnc.b_out1280x1440
     },
     1440:{
-        max:true,
-        min:false,
         in:fnc.b_in1440x1600,
         out:fnc.b_out1440x1600
     },
     1600:{
-        max:true,
-        min:false,
         in:fnc.b_in1600,
         out:fnc.b_out1600
-    }
+    },
+    max:true,
+    min:false
 });
 
 //include exclude
